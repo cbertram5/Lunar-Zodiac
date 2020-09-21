17398c28d720c586e4921bdfda34fbd38046383e
Chris Bertram <bertramc@bc.edu>
2020-09-21 17:01:58 -0400

    class adjustments

--- Lunar Zodiac/ViewController.swift
+++ Lunar Zodiac/ViewController.swift
@@ -13,33 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var animalImageView: UIImageView!
     var currentYear = 2020
     var currentImageNumber = 0
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         animalImageView.image = UIImage(named: "image0")
-        
+        yearLabel.text = "\(currentYear)"
     }
     
-    @IBAction func leftButtonPressed(_ sender: UIButton) {
-       currentImageNumber -= 1
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        currentImageNumber = currentImageNumber + sender.tag
         if currentImageNumber < 0 {
             currentImageNumber = 11
-        }
-        animalImageView.image = UIImage(named: "image\(currentImageNumber)")
-        currentYear = currentYear - 1
-        yearLabel.text = "\(currentYear)"
-    }
-    @IBAction func rightButtonPressed(_ sender: UIButton) {
-        currentImageNumber += 1
-        if currentImageNumber > 11 {
+        } else if currentImageNumber > 11 {
             currentImageNumber = 0
         }
-        animalImageView.image = UIImage(named: "image\(currentImageNumber)")
-        currentYear += 1
-        yearLabel.text = "\(currentYear)"
-    }
-    
 
+        animalImageView.image = UIImage(named: "image\(currentImageNumber)")
+        
+        currentYear = currentYear + sender.tag
+        yearLabel.text = "\(currentYear)"
+        
+        }
+    
 }
 
