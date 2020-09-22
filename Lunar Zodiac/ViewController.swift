9f6e4c080643bb3573482284a5a59bc21c9e8c07
Chris Bertram <bertramc@bc.edu>
2020-09-21 21:19:16 -0400

    Lunar zodiac 9-21 edits

--- Lunar Zodiac/ViewController.swift
+++ Lunar Zodiac/ViewController.swift
@@ -11,14 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var animalImageView: UIImageView!
+    @IBOutlet weak var animalNameLabel: UILabel!
+    
     var currentYear = 2020
     var currentImageNumber = 0
-    
+    let animalNames = ["RAT", "OX", "TIGER", "RABBIT", "DRAGON", "SNAKE", "HORSE", "GOAT", "MONKEY", "ROOSTER", "DOG", "PIG"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        animalImageView.image = UIImage(named: "image0")
         yearLabel.text = "\(currentYear)"
+        animalImageView.image = UIImage(named: animalNames[currentImageNumber])
+        animalNameLabel.text = animalNames[currentImageNumber]
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
@@ -28,8 +31,10 @@ class ViewController: UIViewController {
         } else if currentImageNumber > 11 {
             currentImageNumber = 0
         }
+        
+        animalNameLabel.text = animalNames[currentImageNumber]
 
-        animalImageView.image = UIImage(named: "image\(currentImageNumber)")
+        animalImageView.image = UIImage(named: animalNames[currentImageNumber])
         
         currentYear = currentYear + sender.tag
         yearLabel.text = "\(currentYear)"
